7ca8437c68523a37c62a243dadab3e03f13228ef
saurabhbisht621 <saurabhbisht621@gmail.com>
2025-07-25 14:47:31 +0530

    Add files via upload

---
+++ scripts/checkout.js
@@ -0,0 +1,6 @@
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentsummary } from "./checkout/paymentSummary.js";
+import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
+renderCheckoutHeader();
+renderOrderSummary();
+renderPaymentsummary();
